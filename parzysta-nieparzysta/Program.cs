50b2cec540a0968321343e62f726254e16bbdcf1
arkis0 <kontotak163@gmail.com>
2022-12-07 19:51:43 +0100

    Project

---
+++ parzysta-nieparzysta/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parzysta_nieparzysta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Podaj liczbę: ");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine($"Liczba {number} jest podzielna przez dwa.");
+                }
+                else
+                {
+                    Console.WriteLine($"Liczba {number} nie jest podzielna przez dwa.");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
